601d52a2ef1c790e603a91373a2f7d20d569647b
Arun Deep Singh Choudhary <arundev8153@gmail.com>
2021-12-02 23:00:40 +0530

    json

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ var textTranslate = document.querySelector("#txt-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#txt-output");
 
-
 function clickHandler() {
-    outputDiv.innerText = "Arun Loves " + txtInput.value;
+    outputDiv.innerText = "tanay " + txtInput.value;
 };
 
 textTranslate.addEventListener("click", clickHandler)
